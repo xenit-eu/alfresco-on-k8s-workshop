c5ec90aea2052e8480ae20c68b8e2fe4c0963644
Thijs Lemmens <thijs.lemmens@xenit.eu>
2022-10-19 09:53:13 +0200

    first commit

---
+++ ubuntu-install/install.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env sh
+
+
+# install docker
+apt-get update
+
+apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# install kind
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.16.0/kind-linux-amd64
+chmod +x ./kind
+mv ./kind /usr/local/bin/kind
+
+# install kubectl
+snap install kubectl --classic
+
+# setup cluster
+kind create cluster --config --config=kind/config.yaml
+kubectl config use-context kind-kind
+
+# install ingress controller
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+# install helm
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+# install k9s
+curl -sS https://webi.sh/k9s | sh
+source ~/.config/envman/PATH.env
