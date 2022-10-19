1f5123b98d4ec30da3e5b5b28193d5b554e63fe3
joost.de.cupere@xenit.eu <joost.de.cupere@xenit.eu>
2022-10-19 17:03:40 +0200

    Add the jetstack helm repo

--- ubuntu-install/install.sh
+++ ubuntu-install/install.sh
@@ -41,6 +41,8 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
 # install cert manager
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
 helm install \
   cert-manager jetstack/cert-manager \
   --namespace cert-manager \
